a257226154d582381954b6f7d83faed07ea7afd5
Fernando Maclen <fmaclen@gmail.com>
2023-11-25 15:37:53 -0500

    deploy with cloudflare config
    Deploying to CloudFlare Pages won't work at the moment because the app relies in a file system to save the sessions.
    
    Converting that behavior to use the browser's localStorage could allow this app to run on CF Pages.

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,15 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
+export default {
 	preprocess: vitePreprocess(),
-
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter({
+			// See below for an explanation of these options
+			routes: {
+				include: ['/*'],
+				exclude: ['<all>']
+			}
+		})
 	}
 };
-
-export default config;
