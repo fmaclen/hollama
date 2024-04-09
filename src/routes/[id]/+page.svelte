bbcfbb596b0dd186cbf1ed431b9302ccf0191c5e
Fernando Maclen <fmaclen@gmail.com>
2024-04-09 10:29:11 -0400

    feat: new ui with shadcn (#11)
    * feat: new ui with shadcn
    
    * add button, resizable, textarea components
    
    * add tailwind styling for session view
    
    * add input, select components
    
    * remove resizable pane from layout
    
    * add label component
    
    * working implementation of server and model selection page
    
    wip styling
    
    * add positive, warning variants to Button
    
    * settings page styling tweaks
    
    * homepage styles cleanup
    
    * more style improvements
    
    * add styling for unordered list
    
    * restore ollamaURL.origin
    
    * fix error handling in session page
    
    * various style cleanups
    
    * make favicon transparent and compressed

--- src/routes/[id]/+page.svelte
+++ src/routes/[id]/+page.svelte
@@ -1,48 +1,33 @@
 <script lang="ts">
-	import MarkdownIt from 'markdown-it';
+	import * as Resizable from '$lib/components/ui/resizable';
+	import Button from '$lib/components/ui/button/button.svelte';
+	import Separator from '$lib/components/ui/separator/separator.svelte';
+	import Textarea from '$lib/components/ui/textarea/textarea.svelte';
+
 	import { ollamaGenerate } from '$lib/ollama';
 	import { saveSession, type Message, type Session, loadSession } from '$lib/sessions';
 	import type { PageData } from './$types';
+	import Article from './Article.svelte';
 
 	export let data: PageData;
-
-	const md = new MarkdownIt();
 
 	let messageWindow: HTMLElement;
 	let session: Session;
 	let completion: string;
 	let prompt: string;
-	let isSpeaking: boolean = false;
 
 	$: session = loadSession(data.id);
+	$: isLastMessageFromUser = session.messages[session.messages.length - 1]?.role === 'user';
 
 	function scrollToBottom() {
 		messageWindow.scrollTop = messageWindow.scrollHeight;
 	}
 
-	function speak() {
-		if (isSpeaking) {
-			speechSynthesis.cancel();
-			isSpeaking = false;
-			return;
-		}
-
-		const message = session.messages[session.messages.length - 1].content;
-		const utterance = new SpeechSynthesisUtterance(message);
-		utterance.rate = 0.8;
-		if (/iPad|iPhone|iPod/.test(navigator.userAgent)) {
-			const voices = speechSynthesis.getVoices();
-			utterance.voice = voices.find((voice) => voice.name === 'Samantha') || voices[0];
-		}
-		speechSynthesis.speak(utterance);
-		isSpeaking = true;
-	}
-
 	function handleError(error: any) {
-		console.error(error);
+		// console.error(error);
 		const message: Message = {
 			role: 'system',
-			content: error ? error : 'Sorry, something went wrong.'
+			content: typeof(error) === 'string' ? error : 'Sorry, something went wrong.'
 		};
 		session.messages = [...session.messages, message];
 	}
@@ -104,233 +89,58 @@
 			handleError(error);
 		}
 	}
+
+	const _a = `
+		underline
+		underline-offset-4
+		hover:text-neutral-500
+	`;
 </script>
 
-<div class="chat">
-	<header class="chat__header">
-		<a href="/" title="Main menu">
-			<img class="chat__logo" src="/favicon.png" alt="Ollama" width="48" height="48" />
-		</a>
-		<div class="chat__model">
-			<p class="chat__model-name">Session: <a href={`/${session.id}`}>{session.id}</a></p>
-			<p class="chat__model-label">Model: {session.model}</p>
-		</div>
+<div class="h-screen w-full flex flex-col">
+	<div class="space-y-1 px-6 py-4">
+		<p class="text-sm leading-none">
+			Session <a class={_a} href={`/${session.id}`}>#{session.id}</a>
+		</p>
+		<p class="text-sm text-muted-foreground">{session.model}</p>
+	</div>
 
-		<nav class="chat__modes">
-			<!-- <button title="Keyboard" class="chat__modes-button chat__modes-button--active">⌨️</button> -->
-			<button
-				title="Voice"
-				class="chat__modes-button {isSpeaking && 'chat__modes-button--is-speaking'}"
-				on:click={speak}>🎧</button
+	<Separator />
+
+	<Resizable.PaneGroup direction="horizontal">
+		<Resizable.Pane defaultSize={40} minSize={30}>
+			<div class="flex h-full flex-col gap-y-6 bg-gray-100 p-6">
+				<Textarea
+					placeholder="Prompt"
+					class="h-full resize-none"
+					bind:value={prompt}
+					on:keydown={handleKeyDown}
+				/>
+				<Button on:click={handleSubmit} disabled={!prompt}>Send</Button>
+			</div>
+		</Resizable.Pane>
+		<Resizable.Handle />
+		<Resizable.Pane defaultSize={40} minSize={30}>
+			<div
+				class="h-full flex flex-col gap-y-4 overflow-y-auto bg-gray-100 p-6 text-neutral-800"
+				bind:this={messageWindow}
 			>
-		</nav>
-	</header>
+				{#each session.messages as message, i}
+					{@const isFirst = i === 0}
+					{@const isLast = i === session.messages.length - 1}
+					<Separator class={isFirst ? 'opacity-0' : ''} />
+					<Article {message} />
+					{#if isLast && !isLastMessageFromUser}
+						<Separator class="opacity-0" />
+					{/if}
+				{/each}
 
-	<main class="chat__messages" bind:this={messageWindow}>
-		{#each session.messages as message}
-			<article class="chat__article">
-				<p class="chat__role chat__role--{message.role}">{message.role}</p>
-				<div class="markdown">
-					{@html md.render(message.content)}
-				</div>
-			</article>
-		{/each}
-
-		{#if session.messages[session.messages.length - 1]?.role === 'user'}
-			<article class="chat__article">
-				<p class="chat__role chat__role--ai">AI</p>
-				{#if completion}
-					<div class="markdown">
-						{@html md.render(completion)}
-					</div>
-				{:else}
-					<p class="chat__message">...</p>
+				{#if isLastMessageFromUser}
+					<Separator />
+					<Article message={{ role: 'ai', content: completion || '...' }} />
+					<Separator class="opacity-0" />
 				{/if}
-			</article>
-		{/if}
-	</main>
-
-	<footer class="chat__footer">
-		<div class="chat__type">
-			<textarea
-				class="chat__textarea"
-				placeholder="Type your message here"
-				bind:value={prompt}
-				on:keydown={handleKeyDown}
-			/>
-			<button on:click={handleSubmit}>Send</button>
-		</div>
-	</footer>
+			</div>
+		</Resizable.Pane>
+	</Resizable.PaneGroup>
 </div>
-
-<style lang="scss">
-	.chat {
-		--color-border: rgba(0, 0, 0, 0.1);
-		--color-active: #007aff;
-
-		display: grid;
-		width: 100%;
-		height: 100dvh;
-		grid-template-rows: max-content auto max-content;
-
-		&__header,
-		&__messages {
-			padding-block: 16px;
-			padding-inline: 24px;
-			background-color: var(--color-background);
-		}
-
-		&__header {
-			display: flex;
-			gap: 16px;
-			align-items: center;
-			border-bottom: 1px solid var(--color-border);
-			position: sticky;
-			top: 0;
-		}
-
-		&__logo {
-			display: block;
-		}
-
-		&__modes {
-			display: flex;
-			gap: 8px;
-			margin-left: auto;
-		}
-
-		&__modes-button {
-			border: 1px solid var(--color-border);
-			background-color: unset;
-			padding: 6px;
-			font-size: 18px;
-
-			&--is-speaking {
-				background-color: var(--color-active);
-			}
-		}
-
-		&__messages {
-			overflow-y: auto;
-		}
-
-		&__article {
-			margin-block: 24px;
-			display: grid;
-			grid-template-columns: 72px auto;
-		}
-
-		&__role {
-			font-weight: 600;
-			margin-block: unset;
-			text-transform: uppercase;
-			font-size: 12px;
-			letter-spacing: 1px;
-			line-height: 20px;
-
-			&--user {
-				color: rgba(0, 0, 0, 0.33);
-			}
-		}
-
-		&__message {
-			margin-block: unset;
-			white-space: pre-wrap;
-		}
-
-		&__model {
-			display: flex;
-			flex-direction: column;
-			gap: 4px;
-		}
-
-		&__model-label,
-		&__model-name {
-			font-size: 12px;
-			margin-block: unset;
-		}
-
-		&__model-label {
-			color: rgba(0, 0, 0, 0.33);
-		}
-
-		&__model-name {
-			font-weight: 600;
-
-			a {
-				color: unset;
-			}
-		}
-
-		&__footer {
-			position: sticky;
-			bottom: 0;
-			display: flex;
-			gap: 16px;
-			border-top: 1px solid var(--color-border);
-			background-color: white;
-		}
-
-		&__type {
-			display: flex;
-			width: 100%;
-			gap: 16px;
-
-			textarea,
-			button {
-				padding-block: 24px;
-				padding-inline: 24px;
-				font-size: 16px;
-				border: unset;
-				font-family: var(--font-family);
-			}
-
-			textarea {
-				display: block;
-				width: 100%;
-				min-height: 96px;
-				padding-right: unset;
-				outline: none;
-			}
-
-			button {
-				background-color: unset;
-				padding-left: unset;
-				font-weight: 600;
-				align-self: flex-start;
-				cursor: pointer;
-				color: var(--color-active);
-			}
-		}
-
-		.markdown {
-			width: 100%;
-			overflow: auto;
-
-			:global(p:first-child) {
-				margin-top: unset;
-			}
-
-			:global(p:last-child) {
-				margin-top: unset;
-			}
-
-			:global(code) {
-				background-color: var(--color-border);
-				padding-inline: 6px;
-				padding-block: 2px;
-				opacity: 0.66;
-			}
-
-			:global(pre code) {
-				display: block;
-				overflow-y: auto;
-				width: 100%;
-				padding: 20px;
-				font-size: 13px;
-				margin-block: 12px;
-				box-sizing: border-box;
-			}
-		}
-	}
-</style>
---
+++ src/routes/[id]/Article.svelte
@@ -0,0 +1,54 @@
+<script lang="ts">
+	import MarkdownIt from 'markdown-it';
+	import { type Message } from '$lib/sessions';
+
+	const md = new MarkdownIt();
+
+	export let message: Message;
+</script>
+
+<article class="grid grid-cols-[max-content_auto] gap-x-6">
+	<p class="
+		text-xs
+		text-neutral-900
+		text-center
+		font-bold
+		leading-7
+		uppercase
+		w-[6ch]
+	">
+		{message.role === 'user' ? 'You' : message.role}
+	</p>
+	<div class="
+		overflow-x-auto text-md
+
+		[&>pre>code]:block
+		[&>pre>code]:opacity-75
+		[&>pre>code]:overflow-y-auto
+		[&>pre>code]:bg-gray-300
+		[&>pre>code]:rounded-md
+		[&>pre>code]:text-sm
+		[&>pre>code]:p-4
+		[&>pre:not(:first-child,:last-child)>code]:my-4
+		
+		[&>p]:max-w-[60ch]
+		[&>p:not(:first-child,:last-child)]:my-4
+		[&>ul]:max-w-[60ch]
+		[&>ul]:my-4
+		[&>ul]:flex
+		[&>ul]:flex-col
+		[&>ul]:gap-y-1
+		[&>ul]:list-inside
+		[&>ul]:list-disc
+
+		[&>p>code]:opacity-75
+		[&>p>code]:bg-gray-300
+		[&>p>code]:rounded-md
+		[&>p>code]:text-sm
+		[&>p>code]:p-1
+		[&>p>code]:my-4
+	">
+		{@html md.render(message.content)}
+	</div>
+</article>
+
