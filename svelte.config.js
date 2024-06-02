5fdf8588a4abd2f52f4e9d81cacd84c078f2e610
Fernando Maclen <fmaclen@gmail.com>
2024-06-02 11:53:23 -0400

    fix typo

--- svelte.config.js
+++ svelte.config.js
@@ -23,7 +23,7 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: process.env.ADAPTER = 'docker-node' ?
+		adapter: process.env.ADAPTER === 'docker-node' ?
 			adapterNode(adapterConfig) :
 			adapterCloudflare(adapterConfig)
 	}
