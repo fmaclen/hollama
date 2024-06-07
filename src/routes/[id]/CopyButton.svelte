5fa9184b5fab0dd7511c769717abb87f3ae7f033
Fernando Maclen <fmaclen@gmail.com>
2024-06-07 11:07:44 -0400

    feat: add ability to copy code snippets

---
+++ src/routes/[id]/CopyButton.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import Button from "$lib/components/Button.svelte";
+	import { Files } from "lucide-svelte";
+
+	export let content: string;
+
+	function copyContent() {
+		navigator.clipboard.writeText(content);
+	}
+</script>
+
+<Button title="Copy" variant="icon" size="icon" on:click={copyContent}>
+	<Files class="h-4 w-4" />
+</Button>
