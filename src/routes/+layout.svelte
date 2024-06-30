87b33b55129bcd4b653b3a427ff12222d34b98c1
Fernando Maclen <fmaclen@gmail.com>
2024-06-30 16:32:25 -0400

    styling for top navigation

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,13 @@
 <script lang="ts">
 	import { env } from '$env/dynamic/public';
+	import { page } from '$app/stores';
 
 	import '../app.pcss';
 
 	import Separator from '$lib/components/Separator.svelte';
+	import Button from '$lib/components/Button.svelte';
+
+	$: pathname = $page.url.pathname;
 </script>
 
 <svelte:head>
@@ -20,7 +24,7 @@
 
 <div class="grid h-screen w-screen grid-cols-[max-content,max-content,auto] bg-body text-current">
 	<aside class="flex flex-col justify-between">
-		<a href="/" class="flex flex-col h-screen items-center justify-between py-6 hover:bg-accent">
+		<a href="/" class="flex h-screen flex-col items-center justify-between py-6 hover:bg-accent">
 			<img src="/favicon.png" alt="Hollama logo" width="48" height="48" />
 
 			<h1 class="hollama-wordmark text-2xl font-medium">Hollama</h1>
@@ -30,10 +34,14 @@
 	<Separator orientation="vertical" />
 
 	<main class="grid grid-rows-[max-content,max-content,auto]">
-		<nav class="flex w-max gap-6 p-6 text-sm font-semibold">
-			<a href="/sessions">Sessions</a>
-			<a href="/knowledge">Knowledge</a>
-			<a href="/">Settings</a>
+		<nav class="flex w-max gap-1 px-6 py-3 text-sm font-semibold">
+			<Button variant={pathname === '/sessions' ? 'secondary' : 'outline'} href="/sessions">
+				Sessions
+			</Button>
+			<Button variant={pathname === '/knowledge' ? 'secondary' : 'outline'} href="/knowledge">
+				Knowledge
+			</Button>
+			<Button variant={pathname === '/' ? 'secondary' : 'outline'} href="/">Settings</Button>
 		</nav>
 
 		<Separator />
