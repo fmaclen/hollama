e133e118b15af7caa016544fac23b0ae452ebc7a
SeanAverS <seanasuguitan@gmail.com>
2024-07-03 16:14:00 -0700

    fix: add error page (#55)
    * Add error page with svelte
    
    * Update error page content
    
    ---------
    
    Co-authored-by: Fernando Maclen <fmaclen@gmail.com>

---
+++ src/routes/+error.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import EmptyMessage from '$lib/components/EmptyMessage.svelte';
+</script>
+
+{#if $page.status === 404}
+	<EmptyMessage><strong>Error {$page.status}</strong> — The page you are looking for does not exist.</EmptyMessage>
+{:else if $page.status !== 200}
+	<EmptyMessage><strong>Error {$page.status}</strong> — There was an internal server error. Please try again later.</EmptyMessage>
+{/if}
