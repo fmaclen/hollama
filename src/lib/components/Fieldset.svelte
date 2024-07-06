c3e08a9405d59afc50c545d0e7dcb847bc8e783c
Fernando Maclen <fmaclen@gmail.com>
2024-07-06 17:57:13 -0400

    fix: consolidate widths of form elements and completions (#69)
    * fix: consolidate width of form elements and completions
    
    * Update Fieldset.svelte
    
    * update docs

--- src/lib/components/Fieldset.svelte
+++ src/lib/components/Fieldset.svelte
@@ -8,7 +8,7 @@
 
 <style lang="scss">
 	.fieldset {
-		@apply container mx-auto flex max-w-full flex-col gap-y-6 xl:max-w-[70%];
+		@apply container mx-auto flex max-w-full xl:max-w-[96ch] flex-col gap-y-6;
 
 		&--fullscreen {
 			@apply h-full py-6 overflow-hidden;
