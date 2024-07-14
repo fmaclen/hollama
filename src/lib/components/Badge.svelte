ded8aeed66c80f6e6b71b3833933fa618553eb43
Fernando Maclen <fmaclen@gmail.com>
2024-07-13 22:33:05 -0400

    fix: minor re-styling of the session view (#89)
    - Increase contrast between the session roles
    - Colorize short code blocks
    - Hide copy button on mobile, closes #84
    - Other minor styling enhancements

--- src/lib/components/Badge.svelte
+++ src/lib/components/Badge.svelte
@@ -1,17 +1,27 @@
 <script lang="ts">
 	let className: string | undefined | null = undefined;
 	export let href: string | undefined = undefined;
-	export let variant: 'positive' | 'warning';
+	export let variant: 'default' | 'positive' | 'warning' | undefined = 'default';
 	export { className as class };
+	export let capitalize: boolean = true;
 </script>
 
-<svelte:element this={href ? 'a' : 'span'} class="badge badge--{variant} {className}" {href}>
+<svelte:element
+	this={href ? 'a' : 'span'}
+	class="badge badge--{variant} {className}"
+	{href}
+	style={capitalize ? 'text-transform: capitalize;' : ''}
+>
 	<slot />
 </svelte:element>
 
 <style lang="scss">
 	.badge {
-		@apply inline-flex select-none items-center rounded-md px-2.5 py-0.5 font-mono text-xs capitalize;
+		@apply inline-flex select-none items-center rounded-md px-2.5 py-0.5 font-mono text-xs;
+
+		&--default {
+			@apply border border-shade-3 text-muted;
+		}
 
 		&--positive {
 			@apply bg-positive-muted text-positive;
