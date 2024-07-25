20b39847bfc324c884a15f4ccd2ea36a147500e5
Gregorio Maclen <gregorio.maclen@gmail.com>
2024-07-25 11:20:52 -0300

    remove unused import

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
 	import { env } from '$env/dynamic/public';
 	import { page } from '$app/stores';
 	import { Brain, MessageSquareText, Settings2, Sun, Moon } from 'lucide-svelte';
