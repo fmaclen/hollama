81da984577c4f17eec839e9c7e6aedd8060adf6b
Gregorio Maclen <gregorio.maclen@gmail.com>
2024-08-03 17:30:18 -0300

    remove adapter static

--- svelte.config.js
+++ svelte.config.js
@@ -3,33 +3,15 @@ dotenv.config();
 
 import adapterNode from '@sveltejs/adapter-node';
 import adapterCloudflare from '@sveltejs/adapter-cloudflare';
-import adapterStatic from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-let adapter = adapterNode({
+const adapterConfig = {
+	// See below for an explanation of these options
 	routes: {
 		include: ['/*'],
 		exclude: ['<all>']
 	}
-});
-// if (process.env.ADAPTER === 'electron-node') {
-// adapter = adapterStatic({
-// 	pages: 'build',
-// 	assets: 'build',
-// 	fallback: 'index.html'
-// });
-// } else {
-// 	const adapterConfig = {
-// 		// See below for an explanation of these options
-// 		routes: {
-// 			include: ['/*'],
-// 			exclude: ['<all>']
-// 		}
-// 	};
-
-// 	if (process.env.ADAPTER === 'docker-node') adapter = adapterNode(adapterConfig);
-// 	else adapter = adapterCloudflare(adapterConfig);
-// }
+};
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -41,7 +23,9 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter,
+		adapter: ['docker-node', 'electron-node'].includes(process.env.ADAPTER)
+			? adapterNode(adapterConfig)
+			: adapterCloudflare(adapterConfig),
 		version: {
 			name: process.env.npm_package_version
 		}
