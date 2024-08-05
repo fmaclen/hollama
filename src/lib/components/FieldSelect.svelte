04a716fe63029be12c6bc486d8c916a8811ced5f
Fernando Maclen <fmaclen@gmail.com>
2024-08-05 08:26:43 -0400

    fix: field select styling issue in Safari (#138)
    Closes #137

--- src/lib/components/FieldSelect.svelte
+++ src/lib/components/FieldSelect.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { ChevronDown } from 'lucide-svelte';
 	import Field from './Field.svelte';
 
 	export let name: string;
@@ -10,16 +11,23 @@
 
 <Field {name} {disabled}>
 	<svelte:fragment slot="label">{label}</svelte:fragment>
-	<select id={name} class="select" {disabled} bind:value>
-		<option></option>
-		{#each options as { value, option }}
-			<option {value}>{option}</option>
-		{/each}
-	</select>
+	<div class="select-container">
+		<select id={name} class="select" {disabled} bind:value>
+			<option></option>
+			{#each options as { value, option }}
+				<option {value}>{option}</option>
+			{/each}
+		</select>
+		<ChevronDown class="bg-base -ml-2 mb-2 mr-2 h-4 w-4" />
+	</div>
 </Field>
 
 <style lang="postcss">
+	.select-container {
+		@apply flex items-center;
+	}
+
 	.select {
-		@apply base-input -ml-1;
+		@apply base-input cursor-pointer appearance-none;
 	}
 </style>
