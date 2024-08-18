f58f82e052c3aff5099728604c9bbddec9169c3d
Fernando Maclen <fmaclen@gmail.com>
2024-08-17 21:58:15 -0400

    fix linting issue

--- src/lib/ollama.ts
+++ src/lib/ollama.ts
@@ -1,11 +1,6 @@
 import { get } from 'svelte/store';
 import { settingsStore } from '$lib/store';
-import type {
-	ErrorResponse,
-	ProgressResponse,
-	PullRequest,
-	StatusResponse
-} from 'ollama/browser';
+import type { ErrorResponse, ProgressResponse, PullRequest, StatusResponse } from 'ollama/browser';
 
 export type OllamaModel = {
 	name: string;
