369f8cbac8cb3140af0dc21031bc729729a3b5e4
Gregorio Maclen <gregorio.maclen@gmail.com>
2024-08-25 15:12:41 -0300

    use clearer syntax

--- src/routes/sessions/[id]/+page.svelte
+++ src/routes/sessions/[id]/+page.svelte
@@ -172,9 +172,7 @@
 	async function scrollToBottom(shouldForceScroll = false) {
 		if (!shouldForceScroll && (!messageWindow || userScrolledUp)) return;
 		await tick();
-		requestAnimationFrame(() => {
-			messageWindow.scrollTop = messageWindow.scrollHeight;
-		});
+		requestAnimationFrame(() => (messageWindow.scrollTop = messageWindow.scrollHeight));
 	}
 
 	function handleError(error: Error) {
