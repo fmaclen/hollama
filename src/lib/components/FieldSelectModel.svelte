473352de77154606569ae09e233230958fb12917
Fernando Maclen <fmaclen@gmail.com>
2024-09-08 18:42:29 -0400

    wip: add bits-ui combobox

--- src/lib/components/FieldSelectModel.svelte
+++ src/lib/components/FieldSelectModel.svelte
@@ -1,19 +1,65 @@
 <script lang="ts">
+	import { Combobox, Label, type Selected } from 'bits-ui';
+	import { Check, ChevronDown } from 'lucide-svelte';
+
 	import LL from '$i18n/i18n-svelte';
 	import { settingsStore } from '$lib/localStorage';
 
-	import FieldSelect from './FieldSelect.svelte';
+	import Fruits from './Fruits.svelte';
 
-	export let disabled: boolean = false;
+	const INPUT_ID = 'model';
+	let inputValue = '';
+	let selected: Selected<string> | undefined;
 
-	let value: string = $settingsStore.ollamaModel || '';
-	$: $settingsStore.ollamaModel = value;
+	// $: console.log('ollamaModels', $settingsStore.ollamaModels);
+	// $: console.log('ollamaModel', $settingsStore.ollamaModel);
+	$: console.log('selected', selected);
+	$: console.log('$settingsStore.ollamaModel', $settingsStore.ollamaModel);
+
+	$: models = $settingsStore.ollamaModels.map((m) => ({ value: m.name, label: m.name }));
+	$: if ($settingsStore.ollamaModel)
+		selected = { value: $settingsStore.ollamaModel, label: $settingsStore.ollamaModel };
+	$: filteredModels = inputValue ? models.filter((m) => m.value.includes(inputValue)) : models;
+	$: $settingsStore.ollamaModel = inputValue.trim();
 </script>
 
-<FieldSelect
-	label={$LL.availableModels()}
-	name="model"
-	options={$settingsStore.ollamaModels.map((m) => ({ value: m.name, option: m.name }))}
-	disabled={disabled || !$settingsStore.ollamaModels.length}
-	bind:value
-/>
+<Label.Root
+	for={INPUT_ID}
+	class="flex w-full flex-col gap-y-1 rounded-md border bg-shade-0 text-sm"
+>
+	<div class="flex items-center gap-x-2 px-3 pb-0.5 pt-3 text-xs font-medium leading-none">
+		{$LL.availableModels()}
+	</div>
+
+	<Combobox.Root
+		{selected}
+		items={filteredModels}
+		bind:inputValue
+		disabled={!$settingsStore.ollamaModels.length}
+	>
+		<div class="flex pr-3">
+			<Combobox.Input
+				id={INPUT_ID}
+				class="base-input text-sm"
+				placeholder="Search a model"
+				aria-label="Search a model"
+			/>
+			<ChevronDown class="h-4 w-4" />
+		</div>
+
+		<Combobox.Content class="mt-1 rounded-md bg-shade-0 shadow-md">
+			{#each models as model (model.value)}
+				<Combobox.Item value={model} class="data-[highlighted]:bg-accent-muted">
+					{model.label}
+					<Combobox.ItemIndicator>
+						<Check class="h-4 w-4" />
+					</Combobox.ItemIndicator>
+				</Combobox.Item>
+			{:else}
+				<span>No results found</span>
+			{/each}
+		</Combobox.Content>
+	</Combobox.Root>
+</Label.Root>
+
+<Fruits />
