3388451b069f07274c7dad7fc9defaaee0183f95
Fernando Maclen <fmaclen@gmail.com>
2024-09-18 11:37:14 -0400

    chore: limit test runs to 1 worker in CI

--- playwright.config.ts
+++ playwright.config.ts
@@ -8,6 +8,7 @@ const config: PlaywrightTestConfig = {
 	testDir: 'tests',
 	testMatch: /(.+\.)?(test|spec)\.[jt]s/,
 	timeout: 5000,
+	workers: process.env.CI ? 1 : undefined,
 	use: {
 		trace: 'retain-on-failure',
 		contextOptions: {
