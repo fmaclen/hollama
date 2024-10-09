8468d087ef98ab8dd22d5902cc926d1a7445b782
Gregorio Maclen <gregorio.maclen@gmail.com>
2024-10-09 12:23:25 -0300

    retrieve models in sessions page

--- src/routes/sessions/+page.svelte
+++ src/routes/sessions/+page.svelte
@@ -1,7 +1,15 @@
 <script>
+	import { onMount } from 'svelte';
+
 	import LL from '$i18n/i18n-svelte';
+	import { listModels } from '$lib/chat';
 	import Empty from '$lib/components/EmptyMessage.svelte';
 	import Head from '$lib/components/Head.svelte';
+	import { settingsStore } from '$lib/localStorage';
+
+	onMount(async () => {
+		$settingsStore.models = await listModels();
+	});
 </script>
 
 <Head title={$LL.sessions()} />
