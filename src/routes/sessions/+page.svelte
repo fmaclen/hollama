2ed7cb7ad60ccfcc49b7b25919bb85b5859e936c
Gregorio Maclen <gregorio.maclen@gmail.com>
2024-10-22 17:48:13 -0300

    move fetch models to +layout instead of +page

--- src/routes/sessions/+page.svelte
+++ src/routes/sessions/+page.svelte
@@ -1,16 +1,7 @@
 <script>
-	import { onMount } from 'svelte';
-
 	import LL from '$i18n/i18n-svelte';
-	import { getLastUsedModels, listModels } from '$lib/chat';
 	import Empty from '$lib/components/EmptyMessage.svelte';
 	import Head from '$lib/components/Head.svelte';
-	import { settingsStore } from '$lib/localStorage';
-
-	onMount(async () => {
-		$settingsStore.models = await listModels();
-		$settingsStore.lastUsedModels = getLastUsedModels();
-	});
 </script>
 
 <Head title={$LL.sessions()} />
