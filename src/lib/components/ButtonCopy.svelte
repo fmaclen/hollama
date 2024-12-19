fa3c02e533d8721c9306303ba90fca198bff2232
Fernando Maclen <fmaclen@gmail.com>
2024-12-19 15:10:46 -0500

    hack: add sketchy copy

--- src/lib/components/ButtonCopy.svelte
+++ src/lib/components/ButtonCopy.svelte
@@ -7,8 +7,46 @@
 
 	export let content: string;
 
-	function copyContent() {
-		navigator.clipboard.writeText(content);
+	async function copyContent() {
+		let result = false;
+		if (!navigator.clipboard) {
+			const textArea = document.createElement('textarea');
+			textArea.value = content;
+
+			// Avoid scrolling to bottom
+			textArea.style.top = '0';
+			textArea.style.left = '0';
+			textArea.style.position = 'fixed';
+
+			document.body.appendChild(textArea);
+			textArea.focus();
+			textArea.select();
+
+			try {
+				const successful = document.execCommand('copy');
+				const msg = successful ? 'successful' : 'unsuccessful';
+				console.log('Fallback: Copying text command was ' + msg);
+				result = true;
+			} catch (err) {
+				console.error('Fallback: Oops, unable to copy', err);
+			}
+
+			document.body.removeChild(textArea);
+			return result;
+		}
+
+		result = await navigator.clipboard
+			.writeText(content)
+			.then(() => {
+				console.log('Async: Copying to clipboard was successful!');
+				return true;
+			})
+			.catch((error) => {
+				console.error('Async: Could not copy text: ', error);
+				return false;
+			});
+
+		return result;
 	}
 </script>
 
