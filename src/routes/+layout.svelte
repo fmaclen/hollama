a111db988aba3b261dc6261500268370d5b2f6b5
aginies <aginies@suse.com>
2025-02-11 17:20:48 +0100

    add FR

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -33,7 +33,7 @@
 		if (!$settingsStore.userLanguage)
 			$settingsStore.userLanguage = detectLocale(
 				'en',
-				['en', 'de', 'zh-cn', 'es', 'pt-br', 'ja', 'tr', 'vi'],
+				['en', 'de', 'zh-cn', 'es', 'fr', 'pt-br', 'ja', 'tr', 'vi'],
 				navigatorDetector
 			) as Locales;
 
