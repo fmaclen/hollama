e1f47b0e1cd4b18aa00447a953136aa311c05cb4
Fernando Maclen <fmaclen@gmail.com>
2025-05-11 14:10:05 -0400

    feat: add support for legend snippets in <Fieldset>

--- src/lib/components/Fieldset.svelte
+++ src/lib/components/Fieldset.svelte
@@ -2,18 +2,33 @@
 	import type { Snippet } from 'svelte';
 
 	interface Props {
+		legend?: Snippet;
 		children?: Snippet;
 	}
 
-	let { children }: Props = $props();
+	let { legend, children }: Props = $props();
 </script>
 
-<fieldset class="fieldset">
+<fieldset class="fieldset {legend ? 'fieldset--with-legend' : ''}">
+	{#if legend}
+		<legend class="fieldset__legend">
+			{@render legend?.()}
+		</legend>
+	{/if}
+
 	{@render children?.()}
 </fieldset>
 
 <style lang="postcss">
 	.fieldset {
 		@apply container mx-auto flex max-w-[80ch] flex-col gap-y-3;
+	}
+
+	.fieldset--with-legend {
+		@apply rounded-md border border-shade-4 p-4;
+	}
+
+	.fieldset__legend {
+		@apply flex items-stretch gap-x-2;
 	}
 </style>
